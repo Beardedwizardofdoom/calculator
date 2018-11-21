953a06ecd5ab0cb9d18b74da16422d4198b99031
Beardedwizardofdoom <contact@darylboyce.co.uk>
2018-11-21 22:16:01 +0000

    Added basic buttons and started work on the javascript.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let num1;
+let num2;
+
+
+const userInput = document.getElementById("userinput");
+const addButton = document.getElementById("add-button");
+const subtractButton = document.getElementById("subtract-button");
+const multiplyButton = document.getElementById("multiply-button");
+const devisionButton = document.getElementById("devision-button");
+
+addButton.addEventListener("click",);
+console.log();
+
+function plus(){}
+function minus(){}
+function times(){}
+function devide(){}
+
+
+
+
+// function myFunction() {
+//     document.getElementById("userinput").reset();
+// }
