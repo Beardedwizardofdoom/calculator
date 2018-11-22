ae6bd1577b703f6b3e1388a268d464fcc0aafd76
pookage <pookagehayes@gmail.com>
2018-11-22 21:06:36 +0000

    loaded functionality after domcontent loaded, and moved out of global scope

--- js/script.js
+++ js/script.js
@@ -1,52 +1,53 @@
-
-const input1 = document.getElementById("input-1");
-const input2 = document.getElementById("input-2");
-const addButton = document.getElementById("add-button");
-const subtractButton = document.getElementById("subtract-button");
-const multiplyButton = document.getElementById("multiply-button");
-const devisionButton = document.getElementById("devision-button");
-const clearButton = document.getElementById("clear-button");
-const output1 = document.getElementById("output1");
+window.addEventListener("DOMContentLoaded", initCalculator);
 
 
-addButton.addEventListener("click", plus);
-subtractButton.addEventListener("click", minus);
-multiplyButton.addEventListener("click", times);
-devisionButton.addEventListener("click", devide);
-clearButton.addEventListener("click", clear);
+function initCalculator(){
+
+	const input1 = document.getElementById("input-1");
+	const input2 = document.getElementById("input-2");
+	const addButton = document.getElementById("add-button");
+	const subtractButton = document.getElementById("subtract-button");
+	const multiplyButton = document.getElementById("multiply-button");
+	const devisionButton = document.getElementById("devision-button");
+	const clearButton = document.getElementById("clear-button");
+	const output1 = document.getElementById("output1");
 
 
-function plus(){
+	const monkey = "oooooh";
+
+	addButton.addEventListener("click", plus);
+	subtractButton.addEventListener("click", minus);
+	multiplyButton.addEventListener("click", times);
+	devisionButton.addEventListener("click", devide);
+	clearButton.addEventListener("click", clear);
 
 
-	let addition = parseInt(input1.value) + parseInt(input2.value);
-	
-	output1.textContent = addition;
+	function plus(){
+
+
+		let addition = parseInt(input1.value) + parseInt(input2.value);
+		
+		output1.textContent = addition;
+	}
+	function minus(){
+
+		let subtraction = parseInt(input1.value) - parseInt(input2.value);
+		output1.textContent = subtraction;
+	}
+	function times(){
+
+		let multiply = parseInt(input1.value) * parseInt(input2.value);
+		output1.textContent = multiply;
+	}
+	function devide(){
+
+		let devision = parseInt(input1.value) / parseInt(input2.value);
+		output1.textContent = devision;
+	}
+
+	function clear(){
+		input1.value = "";
+		input2.value = "";
+		output1.value = "";
+	}
 }
-function minus(){
-
-	let subtraction = parseInt(input1.value) - parseInt(input2.value);
-	output1.textContent = subtraction;
-}
-function times(){
-
-	let multiply = parseInt(input1.value) * parseInt(input2.value);
-	output1.textContent = multiply;
-}
-function devide(){
-
-	let devision = parseInt(input1.value) / parseInt(input2.value);
-	output1.textContent = devision;
-}
-
-function clear(){
-	input1.value = "";
-	input2.value = "";
-	output1.value = "";
-}
-
-
-
-// function myFunction() {
-//     document.getElementById("userinput").reset();
-// }
