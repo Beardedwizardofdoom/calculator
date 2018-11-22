b5b5c6e297d1bbdc8c3be5cd8d721cd73407f527
Beardedwizardofdoom <contact@darylboyce.co.uk>
2018-11-22 19:38:06 +0000

    Made all buttons do intended math that displays on page, clear button clears all input and output

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const addButton = document.getElementById("add-button");
 const subtractButton = document.getElementById("subtract-button");
 const multiplyButton = document.getElementById("multiply-button");
 const devisionButton = document.getElementById("devision-button");
-const clearButton = document.getElementById("clear-button")
+const clearButton = document.getElementById("clear-button");
+const output1 = document.getElementById("output1");
 
 
 addButton.addEventListener("click", plus);
@@ -17,28 +18,31 @@ clearButton.addEventListener("click", clear);
 
 function plus(){
 
+
 	let addition = parseInt(input1.value) + parseInt(input2.value);
-	console.log(addition);
+	
+	output1.textContent = addition;
 }
 function minus(){
 
 	let subtraction = parseInt(input1.value) - parseInt(input2.value);
-	console.log(subtraction);
+	output1.textContent = subtraction;
 }
 function times(){
 
 	let multiply = parseInt(input1.value) * parseInt(input2.value);
-	console.log(multiply)	
+	output1.textContent = multiply;
 }
 function devide(){
 
-	let devision = parseInt(input1.value) * parseInt(input2.value);
-	console.log(devision)
+	let devision = parseInt(input1.value) / parseInt(input2.value);
+	output1.textContent = devision;
 }
 
 function clear(){
 	input1.value = "";
 	input2.value = "";
+	output1.value = "";
 }
 
 
